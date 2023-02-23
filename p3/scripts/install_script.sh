99e2d2c3bc7f8b4e5daa1bad5deef44580923081
MySpiritNeverDie <zvmkm@mail.ru>
2023-02-24 00:55:34 +0300

    p3 in progress

---
+++ p3/scripts/install_script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "[SCRIPT] Please run as root"
+  exit
+fi
+
+echo "[SCRIPT] Installing docker"
+pacman -S --needed docker
+
+echo "[SCRIPT] Starting docker service"
+systemctl start docker.service
+systemctl enable docker.service
+
+KUBECTL=/usr/local/bin/kubectl
+if [ -f "$KUBECTL" ]; then
+    echo "[SCRIPT] kubectl already installed"
+else
+    echo "[SCRIPT] Installing kubectl"
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+    echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+    chmod +x kubectl
+    mv ./kubectl /usr/local/bin
+fi
+
+K3D=/usr/local/bin/k3d
+if [ -f "$K3D" ]; then
+    echo "[SCRIPT] k3d already installed"
+else 
+    echo "[SCRIPT] Installing k3d"
+    wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+fi
---
+++ p3/scripts/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+if [ "$EUID" -ne 0 ]
+  then echo -e "${RED}[SCRIPT] Please run as root${NC}"
+  exit
+fi
+
+echo -e "${YELLOW}[SCRIPT] Creating cluster${NC}"
+k3d cluster create dev-cluster --api-port 6443 -p 8080:80@loadbalancer --agents 2 --wait
+
+echo -e "${YELLOW}[SCRIPT] Creating namespaces${NC}"
+kubectl create namespace argocd
+kubectl create namespace dev
+
+echo -e "${YELLOW}[SCRIPT] Applying manifests${NC}"
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+echo -e "${YELLOW}[SCRIPT] Waiting for argocd to be ready${NC}"
+kubectl wait --for=condition=Ready pods --all -n argocd
+
+# echo -e "${YELLOW}[SCRIPT] Applying manifests${NC}"
+# kubectl apply -n argocd -f ../confs/ingress.yaml
+
+# echo -e "${YELLOW}[SCRIPT] Waiting for argocd to be ready${NC}"
+# kubectl wait --for=condition=Ready pods --all -n argocd
+
+echo -e "${YELLOW}[SCRIPT] Retrieving password for argocd${NC}"
+pass=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+echo -e "${YELLOW}Password is ${GREEN}$pass${NC}"
